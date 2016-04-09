ba89482db5d3c462fc909b8c134a4629039519b1
Brian Espinoza <briane@uci.edu>
2016-04-09 00:11:46 -0700

    mp3poolR v.1.4.6
    - Added an option to exclude QuickHitter versions from download queue
    - Download window now displays song’s bpm

--- mp3poolR.R
+++ mp3poolR.R
@@ -1,7 +1,7 @@
 ## Brian Espinoza
 ## briane@uci.edu
 
-mp3poolR <- function(username, pw, path = "", download = TRUE, ask = TRUE){
+mp3poolR <- function(username, pw, path = "", download = TRUE, ask = TRUE, QuickHitter = FALSE){
 
   if(path == "~/Desktop/"){
     stop("choose another folder")
@@ -51,7 +51,7 @@ mp3poolR <- function(username, pw, path = "", download = TRUE, ask = TRUE){
     dir.create(path = download_folder)
   }
 
-  download_list <- tbl_df(data.frame(music_links = NULL, titles = NULL))
+  download_list <- tbl_df(data.frame(music_links = NULL, titles = NULL, bpm = NULL))
 
   make_links_and_titles <- function(session, df){
 
@@ -60,12 +60,18 @@ mp3poolR <- function(username, pw, path = "", download = TRUE, ask = TRUE){
       html_nodes(".innerPlayList1") %>% html_nodes(".play_listing") %>% html_nodes("li") %>%
       html_nodes("audio") %>% html_attr("src")
 
+    bpm2 <- read_html(music_session) %>% html_nodes("div .innerPlayer1") %>%
+      html_nodes(".innerPlayList1") %>% html_nodes(".play_listing") %>% html_nodes("li") %>%
+      html_nodes("div .bpm") %>% html_text()
+
     music_links <- c(df$music_links, (ml))
+    bpm <- c(df$bpm, bpm2)
+
 
     titles <- c(df$titles, (ml %>% str_split("/") %>% sapply(`[`,7) %>%
       str_split("%20") %>% sapply(paste, collapse = " ")))
 
-    download_list <- tbl_df(data_frame(music_links, titles))
+    download_list <- tbl_df(data_frame(music_links, titles, bpm))
 
 
   }
@@ -85,6 +91,11 @@ mp3poolR <- function(username, pw, path = "", download = TRUE, ask = TRUE){
 
 
   download_list <- download_list[(as.integer(which(sapply(X = download_list$titles, FUN = '%in%', logged_tracks) == FALSE))),]
+
+  if (QuickHitter == FALSE){ ## If false, don't download songs with QuickHitter in the title
+    download_list <- download_list[!str_detect(download_list$titles, pattern = "QuickHitter")]
+  }
+
   if (length(download_list$titles) == 0){
     return(message("no new songs to download"))
   }
@@ -99,6 +110,7 @@ mp3poolR <- function(username, pw, path = "", download = TRUE, ask = TRUE){
     download_list[,2]
     for (i in 1:length(download_list$titles)){ # download songs and write to designated file
       cat(download_list$titles[i], paste0("[", i,"/", length(download_list$titles),"]"), sep = "\n")
+      cat(download_list$bpm[i], "\n")
       if (ask == TRUE){
         decision <- readkey()
         if (decision == "y"){
@@ -111,7 +123,7 @@ mp3poolR <- function(username, pw, path = "", download = TRUE, ask = TRUE){
         httr::GET(url = download_list$music_links[i], write_disk(paste0(download_folder, download_list$titles[i])), progress())
         write(download_list$titles[i], file = log_file, append = TRUE)
       }
-      cat("", "", sep = "\n")
+      cat("", "", sep = "\n") # space post-user input
     }
   }
 }
